e0e8b94120faaab6695a0628acba6640bd5ac88d
Medini Patil <patilmedini333@gmail.com>
2025-04-19 15:38:21 +0530

    Added Fragments

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,14 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">A item</li>
-      <li className="list-group-item">A Second item</li>
-      <li className="list-group-item">A Third item</li>
-      <li className="list-group-item">A fourth item</li>
-    </ul>
+    <>
+        <h1>List</h1>
+        <ul className="list-group">
+          <li className="list-group-item">A item</li>
+          <li className="list-group-item">A Second item</li>
+          <li className="list-group-item">A Third item</li>
+          <li className="list-group-item">A fourth item</li>
+        </ul>
+    </>
   );
 }
 
