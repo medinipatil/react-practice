49492b20149d5039986c4e1fe8d2afc696b6480b
Medini Patil <patilmedini333@gmail.com>
2025-04-19 23:38:18 +0530

    Managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,26 @@
-function ListGroup() {
-    let items =[
-        'Mumbai',
-        'London',
-        'China',
-        'Japan'
-    ];
-    items =[];
+import { useState } from "react";
 
-    
+
+function ListGroup() {
+  let items = ["Mumbai", "London", "China", "Japan"];
+ 
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
-        <h1>List</h1>
-        {items.length === 0 && <p>No item found</p>}
-        <ul className="list-group">
-            {items.map(item =>(
-                 <li key={item}>{item}</li>
-            ))}
-        </ul>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? "list-group-item active" : 'list-group-item'}
+            key={item}
+            onClick={()=>{setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
