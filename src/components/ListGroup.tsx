0f2d5e6a549a4d72ae1ca78251673d95dbfada9f
Medini Patil <patilmedini333@gmail.com>
2025-04-19 15:31:34 +0530

    Added List Component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">A item</li>
+      <li className="list-group-item">A Second item</li>
+      <li className="list-group-item">A Third item</li>
+      <li className="list-group-item">A fourth item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
