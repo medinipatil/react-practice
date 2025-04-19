315a98b87ef3e7562df0047c88844f4f8fbc6ef0
Medini Patil <patilmedini333@gmail.com>
2025-04-19 21:36:40 +0530

    conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
 function ListGroup() {
-    const items =[
+    let items =[
         'Mumbai',
         'London',
         'China',
         'Japan'
     ];
+    items =[];
+
     
   return (
     <>
         <h1>List</h1>
+        {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
             {items.map(item =>(
                  <li key={item}>{item}</li>
