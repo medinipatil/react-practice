fb7974a49ee55629d594ad88787c98958aa3dde8
Medini Patil <patilmedini333@gmail.com>
2025-04-20 23:02:56 +0530

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Mumbai", "London", "China", "Japan"];
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  }
+  // let items = ["Mumbai", "London", "China", "Japan"];
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item);
+  // }
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectedItem} />
+      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectedItem} /> */}
+      <Alert>
+        Hello World
+      </Alert>
     </div>
   );
 }
